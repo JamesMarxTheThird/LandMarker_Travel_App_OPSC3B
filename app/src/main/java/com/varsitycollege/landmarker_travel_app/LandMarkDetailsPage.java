acd35e80e124db71bfada0e4a10d8f6367bf241d
JamesMarxTheThird <jmarx1208@gmail.com>
2022-09-28 17:33:14 +0200

    wae

---
+++ app/src/main/java/com/varsitycollege/landmarker_travel_app/LandMarkDetailsPage.java
@@ -0,0 +1,14 @@
+package com.varsitycollege.landmarker_travel_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandMarkDetailsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_land_mark_details_page);
+    }
+}
