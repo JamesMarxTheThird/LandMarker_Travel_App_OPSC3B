1f6a517389f5530f483108ec4af51feb39a0da3b
Byronm123 <93351404+Byronm123@users.noreply.github.com>
2022-10-17 11:45:59 +0200

    Profile and settings

--- app/src/main/java/com/varsitycollege/landmarker_travel_app/ProfilePage.java
+++ app/src/main/java/com/varsitycollege/landmarker_travel_app/ProfilePage.java
@@ -2,36 +2,37 @@ package com.varsitycollege.landmarker_travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 
-
-public class SettingsPage extends AppCompatActivity {
+public class ProfilePage extends AppCompatActivity {
 
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings_page);
+        setContentView(R.layout.activity_profile_page);
 
+        // initialize imageView
+        // with method findViewById()
         imageView = findViewById(R.id.backArrow);
 
-
+        // Apply OnClickListener  to imageView to
+        // switch from one activity to another
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Intent class will help to go to next activity using
                 // it's object named intent.
                 // SecondActivty is the name of new created EmptyActivity.
-                Intent intent = new Intent(SettingsPage.this, HomePage.class);
+                Intent intent = new Intent(ProfilePage.this, SettingsPage.class);
                 startActivity(intent);
             }
         });
-
 
     }
 }
