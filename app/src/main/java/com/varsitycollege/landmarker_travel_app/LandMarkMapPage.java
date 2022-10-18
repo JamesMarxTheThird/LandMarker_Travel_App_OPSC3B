564ab10c9eb42f1d04d4b7a98f84c1251556f30e
AdamCar2022 <adamcar@live.co.za>
2022-10-18 15:42:09 +0200

    Adam first map routing commit #1321124124

--- app/src/main/java/com/varsitycollege/landmarker_travel_app/LandMarkMapPage.java
+++ app/src/main/java/com/varsitycollege/landmarker_travel_app/LandMarkMapPage.java
@@ -12,29 +12,57 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.varsitycollege.landmarker_travel_app.databinding.ActivityLandMarkMapPageBinding;
 
-public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMarkerDragListener, GoogleApiClient.OnConnectionFailedListener, RoutingListener {
 
     private GoogleMap mMap;
     private ActivityLandMarkMapPageBinding binding;
     boolean LocationPermission;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private Location lastKnownLocation;
+    Location myLocation=null;
+    Location destinationLocation=null;
+    //to get location permissions.
+    private final static int LOCATION_REQUEST_CODE = 23;
+    boolean locationPermission=false;
+    private List<Polyline> polylines=null;
+    protected LatLng start=null;
+    protected LatLng end=null;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    //private final LatLng CapeTown = new LatLng(-33.9803833, 18.4759092);
+    private final LatLng CapeTown = new LatLng(-33.9803833, 18.4759092);
     private final LatLng sydney = new LatLng(-34, 151);
+    private double curr_lat, curr_long, end_lat, end_long;
+    private Button getRouteBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +71,21 @@ public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallb
         binding = ActivityLandMarkMapPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //getRouteBTN = findViewById(R.id.calcRouteBtn);
+
+        getLocationPermission();
+
         //fused location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
+
 
     /**
      * Manipulates the map once available.
@@ -60,13 +96,55 @@ public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallb
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    private void getMyLocation(){
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(@NonNull Location location) {
+                myLocation = location;
+                LatLng ltlng = new LatLng(location.getLatitude(), location.getLongitude());
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(ltlng, 16f);
+                mMap.animateCamera(cameraUpdate);
+            }
+        });
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                end = latLng;
+                mMap.clear();
+                start = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                FindRoutes(start, end);
+            }
+        });
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        updateUI();
-        getDeviceLocation();
+        //mMap.setOnMarkerDragListener(this);
+        //mMap.setOnMarkerClickListener(this);
+
+        //updateUI();
+        //getDeviceLocation();
+        getMyLocation();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                end = latLng;
+                mMap.clear();
+
+                start = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+                FindRoutes(start, end);
+            }
+        });
     }
+
 
     private void getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
@@ -82,15 +160,33 @@ public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallb
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         LocationPermission = false;
         if (requestCode
-                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION && requestCode == LOCATION_REQUEST_CODE) {// If request is cancelled, the result arrays are empty.
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 LocationPermission = true;
+                getDeviceLocation();
             }
         } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
         updateUI();
+    }
+
+    public void FindRoutes(LatLng start, LatLng end){
+        if(start==null || end==null) {
+            Toast.makeText(LandMarkMapPage.this,"Unable to get location",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Routing routing = new Routing.Builder()
+                    .travelMode(AbstractRouting.TravelMode.DRIVING)
+                    .withListener(this)
+                    .alternativeRoutes(true)
+                    .waypoints(start, end)
+                    .key("AIzaSyCfmyIKwbi2fCo54F4OIMDslvaSs1q_8OA")  //also define your api key here.
+                    .build();
+            routing.execute();
+        }
     }
 
     private void updateUI() {
@@ -146,5 +242,100 @@ public class LandMarkMapPage extends FragmentActivity implements OnMapReadyCallb
         } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage(), e);
         }
+
+
+
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        marker.setDraggable(true);
+        return false;
+    }
+
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker) {
+        end_lat = marker.getPosition().latitude;
+        end_long = marker.getPosition().longitude;
+    }
+
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar snackbar= Snackbar.make(parentLayout, e.toString(), Snackbar.LENGTH_LONG);
+        snackbar.show();
+//    FindRoutes(start,end);
+    }
+
+    @Override
+    public void onRoutingStart() {
+        Toast.makeText(LandMarkMapPage.this, "Finding Route...", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+        CameraUpdate center = CameraUpdateFactory.newLatLng(start);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+        if(polylines!=null) {
+            polylines.clear();
+        }
+        PolylineOptions polyOptions = new PolylineOptions();
+        LatLng polylineStartLatLng=null;
+        LatLng polylineEndLatLng=null;
+
+
+        polylines = new ArrayList<>();
+        //add route(s) to the map using polyline
+        for (int i = 0; i < route.size(); i++) {
+
+            if(i==shortestRouteIndex)
+            {
+                polyOptions.color(getResources().getColor(R.color.yellow));
+                polyOptions.width(7);
+                polyOptions.addAll(route.get(shortestRouteIndex).getPoints());
+                Polyline polyline = mMap.addPolyline(polyOptions);
+                polylineStartLatLng=polyline.getPoints().get(0);
+                int k=polyline.getPoints().size();
+                polylineEndLatLng=polyline.getPoints().get(k-1);
+                polylines.add(polyline);
+
+            }
+            else {
+
+            }
+    }
+
+        //Add Marker on route starting position
+        MarkerOptions startMarker = new MarkerOptions();
+        startMarker.position(polylineStartLatLng);
+        startMarker.title("My Location");
+        mMap.addMarker(startMarker);
+
+        //Add Marker on route ending position
+        MarkerOptions endMarker = new MarkerOptions();
+        endMarker.position(polylineEndLatLng);
+        endMarker.title("Destination");
+        mMap.addMarker(endMarker);
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+        FindRoutes(start,end);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        FindRoutes(start,end);
     }
 }
